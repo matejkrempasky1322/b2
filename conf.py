5c7b9015a4a169bcdc756e64ac9349e6558abc2c
matej <bobor668@gmail.com>
2026-05-08 23:56:59 +0200

    test

---
+++ conf.py
@@ -0,0 +1,14 @@
+project = 'Interaktívna učebnica fyziky'
+copyright = '2026, Matej Krempaský'
+author = 'Matej Krempaský'
+release = '1.0'
+
+extensions = [
+    'nbsphinx',
+]
+
+language = 'sk'
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = []
