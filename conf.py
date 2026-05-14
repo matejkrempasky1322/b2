43b5830cbdf87400560b918db302efa34557c7a6
matej <bobor668@gmail.com>
2026-05-14 11:02:40 +0200

    update

--- conf.py
+++ conf.py
@@ -5,8 +5,13 @@ release = '1.0'
 
 extensions = [
     'nbsphinx',
+    'sphinx_togglebutton',
+    'sphinx_rtd_dark_mode',
+    'sphinx_copybutton',
+    'sphinx_design',
 ]
 
+default_dark_mode = False
 language = 'sk'
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
